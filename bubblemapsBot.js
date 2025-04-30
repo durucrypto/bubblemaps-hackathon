3a3fa5ed6d11e000a6b9bac2c6b5d47ebe4bdefe
durucrypto <durucrypto@protonmail.com>
2025-04-30 09:38:55 +0300

    Update bubblemapsBot.js

--- bubblemapsBot.js
+++ bubblemapsBot.js
@@ -830,7 +830,7 @@ async function main() {
                     const userMsg = ctx.message.text.trim().replace(/\s+/g, " ");
 
                     if (userMsg.toLowerCase() === "/help" || userMsg.toLowerCase() === "/guide") {
-                        ctx.reply("📘 Here's the how-to-use guide:" + guideUrl);
+                        ctx.reply(`📘 To view the how-to-use guide, [click here](${guideUrl}).`, { parse_mode: "Markdown" });
                         return;
                     }
 
